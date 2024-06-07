e2743d8579e41a33a67dbb7ec3399fb857eed5b9
Kritiiii <142918564+Kritiiguptaa@users.noreply.github.com>
2024-06-07 11:36:25 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -20,6 +20,213 @@ var NorthGoa=["Calangute Beach","Fort Aguada","Anjuna Beach","Chapora Fort","Se 
 
 var SouthGoa=["Basilica of Bom Jesus","Dudhsagar Falls","Colva Beach","Dr. Salim Ali Bird Santuary"]
 
+var Ahmedabad =  ["Sabarmati Ashram", "Sabarmati Riverfront", "Manek Chowk", "Kankaria Lake", "Science City"]
+var Junagadh =  ['Gir National Park']
+var Bhuj =  ['White Desert']
+var Dwarka =  ['Dwarkadhish Temple']
+var Vadodara =  ['Laxmi Vilas Palace']
+var Somnath =  ['Somnath Temple']
+var RannOfKutch =  ['Rann Utsav']
+var Kevadia =  ['Statue of Unity']
+var Gandhinagar =  ['Dandi Kutir', 'Akshardham']
+var Jaipur =  ['Hawa Mahal', 'Amber Fort', 'Jaigarh Fort', 'City Palace', 'Albert Hall Museum']
+var Udaipur =  ['City Palace', 'Lake Pichola']
+var Jaisalmer =  ['Jaisalmer Fort']
+var SawaiMadhopur =  ['Ranthambore National Park']
+var Pushkar =  ['Pushkar Lake']
+var Ajmer =  ['Ajmer Sharif Dargah']
+var Jodhpur =  ['Mehrangarh Fort']
+var Chittorgarh =  ['Chittorgarh Fort']
+var MountAbu =  ['Dilwara Temples']
+var Bikaner =  ['Junagarh Fort']
+var Amritsar =  ['Golden Temple ', 'Jallianwala Bagh', 'Wagah Border']
+var Chandigarh =  ['Rock Garden']
+var Alappuzha =  ['Alappuzha Beach']
+var Munnar =  ['Munnar Tea Gardens']
+var Kochi =  ['Fort Kochi', 'Kerala Folklore Museum', 'Wonderla Amusement Park', 'LuLu International Shopping Mall']
+var Thiruvananthapuram =  ['Padmanabhaswamy Temple']
+var Kozhikode =  ['Kozhikode Beach']
+var Wayanad =  ['Wayanad Wildlife Sanctuary']
+var Thekkady =  ['Periyar National Park']
+var Kumarakom =  ['Kumarakom Bird Sanctuary']
+var Varkala =  ['Varkala Beach']
+var Bekal =  ['Bekal Fort']
+var Kovalam =  ['Kovalam Beach']
+var Kannur =  ['St. Angelo Fort']
+var Nelliyampathy =  ['Seethargundu Viewpoint']
+var Mysore =  ['Mysore Palace']
+var Hampi =  ['Hampi Archaeological Ruins']
+var Coorg =  ['Abbey Falls']
+var Gokarna =  ['Om Beach']
+var Chikmagalur =  ['Mullayanagiri']
+var Badami =  ['Badami Cave Temples']
+var Shivamogga =  ['Jog Falls']
+var Mangalore =  ['Panambur Beach']
+var Murudeshwar =  ['Murudeshwar Temple']
+var Bijapur =  ['Gol Gumbaz']
+var Bandipur =  ['Bandipur National Park']
+var Halebidu =  ['Halebidu Hoysaleswara Temple']
+var Pune =  ['Shaniwar Wada']
+var Aurangabad =  ['Ajanta Caves']
+var Nashik =  ['Sula Vineyards']
+var Shirdi =  ['Sai Baba Temple']
+var Alibaug =  ['Alibaug Beach']
+var Ratnagiri =  ['Ganapatipule Temple']
+var Nagpur =  ['Deekshabhoomi']
+var Kolhapur =  ['Mahalakshmi Temple']
+var Tarkarli =  ['Tarkarli Beach']
+var Satara =  ['Kaas Plateau']
+var Matheran =  ['Echo Point']
+var Ajanta =  ['Ellora Caves']
+var Khajuraho =  ['Khajuraho Group of Monuments']
+var Bhopal =  ['Sanchi Stupa', 'Madhya Pradesh Tribal Museum']
+var Indore =  ['Rajwada Palace']
+var Gwalior =  ['Gwalior Fort']
+var Ujjain =  ['Mahakaleshwar Jyotirlinga']
+var Jabalpur =  ['Dhuandhar Falls']
+var Pachmarhi =  ['Bee Falls']
+var Kanha =  ['Kanha National Park']
+var Bandhavgarh =  ['Bandhavgarh National Park']
+var Orchha =  ['Orchha Fort']
+var Mandu =  ['Jahaz Mahal']
+var Bhimbetka =  ['Bhimbetka Rock Shelters']
+var Amarkantak =  ['Narmada Udgam Temple']
+var Chitrakoot =  ['Chitrakoot Falls']
+var Shimla =  ['The Ridge']
+var Manali =  ['Solang Valley']
+var dalhousie =  ['Dalai Lama Temple', 'Khajjiar Lake']
+var SpitiValley =  ['Key Monastery']
+var Kullu =  ['Great Himalayan National Park']
+var Chamba =  ['Chamera Lake']
+var Kinnaur =  ['Sangla Valley']
+var Kangra =  ['Kangra Fort']
+var Palampur =  ['Tea Gardens']
+var Mandi =  ['Prashar Lake']
+var BirBilling =  ['Paragliding Site']
+var McLeodGanj =  ['Triund Trek']
+var Manikaran =  ['Manikaran Sahib']
+var Narkanda =  ['Hatu Peak']
+var Barot =  ['Barot Valley']
+var Shoja =  ['Serolsar Lake']
+var Kufri =  ['Kufri Fun World']
+var Nainital =  ['Naini Lake']
+var Rishikesh =  ['Laxman Jhula']
+var Haridwar =  ['Har Ki Pauri']
+var Dehradun =  ['Robbers Cave']
+var Mussoorie =  ['Kempty Falls']
+var Auli =  ['Auli Ski Resort']
+var Badrinath =  ['Badrinath Temple']
+var Almora =  ['Binsar Wildlife Sanctuary']
+var Ranikhet =  ['Chaubatia Gardens']
+var JimCorbett =  ['Jim Corbett National Park']
+var Uttarkashi =  ['Gangotri Temple']
+var Chopta =  ['Tungnath Temple']
+var Joshimath =  ['Valley of Flowers']
+var Agra =  ['Taj Mahal', 'Agra Fort']
+var Varanasi =  ['Kashi Vishwanath Temple']
+var Lucknow =  ['Bara Imambara', 'Phoenix Palassio']
+var Mathura =  ['Krishna Janmabhoomi', 'Nand Gaon', 'Barsana Mandir']
+var Ayodhya =  ['Ram Janmabhoomi']
+var Vrindavan =  ['Banke Bihari Temple', 'Prem Mandir']
+var Allahabad =  ['Triveni Sangam']
+var Jhansi =  ['Jhansi Fort']
+var Sarnath =  ['Dhamek Stupa']
+var FatehpurSikri =  ['Buland Darwaza']
+var Noida =  ['Okhla Bird Sanctuary', 'DLF Mall of India']
+var Aligarh =  ['Aligarh Fort']
+var Meerut =  ['Augarnath Temple']
+var Kanpur =  ['Allen Forest Zoo']
+var Srinagar =  ['Dal Lake']
+var Leh =  ['Pangong Tso', 'Thiksey Monastery', 'Magnetic Hill', 'Khardung La Pass', 'Thiksey Monastery']
+var Pahalgam =  ['Betaab Valley']
+var Jammu =  ['Vaishno Devi']
+var Udhampur =  ['Patnitop Height']
+var Anantnag =  ['Amarnath Cave']
+var NubraValley =  ['Nubra Valley']
+var Kargil =  ['Kargil War Memorial']
+var Diskit =  ['Diskit Monastery']
+var Kishtwar =  ['Kishtwar National Park']
+var Hemis =  ['Hemis National Park']
+var Dras =  ['Dras War Memorial']
+var Darjeeling =  ['Tiger Hill']
+var Siliguri =  ['Jaldapara National Park']
+var Sundarbans =  ['Sundarbans National Park']
+var Digha =  ['Digha Beach']
+var Murshidabad =  ['Hazarduari Palace']
+var Bolpur =  ['Kankalitala Temple']
+var Hooghly =  ['Hangseswari Temple']
+var Jalpaiguri =  ['Gorumara National Park']
+var Porbandar =  ['Kirti Mandir']
+var CoochBehar =  ['Cooch Behar Palace']
+var Purulia =  ['Ayodhya Hills']
+var Puri =  ['Jagannath Temple']
+var Konark =  ['Sun Temple']
+var Bhubaneswar =  ['Lingaraj Temple', 'Nandankanan Zoological Park']
+var Rourkela =  ['Khandadhar Waterfall']
+var Cuttack =  ['Barabati Fort']
+var Sambalpur =  ['Hirakud Dam']
+var Chilika =  ['Chilika Lake']
+var Berhampur =  ['Tara Tarini Temple']
+var Keonjhar =  ['Badaghagara Waterfall']
+var Balasore =  ['Chandipur Beach']
+var Kendujhar =  ['Sanaghagara Waterfall']
+var Chennai =  ['Marina Beach']
+var Madurai =  ['Meenakshi Amman Temple']
+var Rameswaram =  ['Ramanathaswamy Temple', 'Ramanathaswamy Temple']
+var Kanyakumari =  ['Vivekananda Rock Memorial']
+var Ooty =  ['Ooty Lake']
+var Coimbatore =  ['Marudamalai Temple']
+var Kodaikanal =  ['Kodaikanal Lake']
+var Thanjavur =  ['Brihadeeswarar Temple']
+var Mahabalipuram =  ['Shore Temple']
+var Yercaud =  ['Yercaud Lake']
+var Tirunelveli =  ['Nellaiappar Temple']
+var Chidambaram =  ['Nataraja Temple']
+var Vijayawada =  ['Kanaka Durga Temple']
+var Visakhapatnam =  ['Rishikonda Beach', 'Kailasagiri', 'Submarine Museum', 'Borra Caves', 'War Memorial', 'Indira Gandhi Zoological Park', 'Matsyadarshini Aquarium', 'Visakha Museum']
+var Srisailam =  ['Mallikarjuna Swamy Temple']
+var Rajahmundry =  ['Papikondalu']
+var Anantapur =  ['Lepakshi']
+var Kurnool =  ['Belum Caves']
+var Amravati =  ['Amaravathi Temple']
+var Guntur =  ['Uppalapadu Bird Sanctuary']
+var Kadapa =  ['Gandikota Fort']
+var Puttaparthi =  ['Prasanthi Nilayam']
+var Vizianagaram =  ['Simhachalam Temple']
+var Gangtok =  ['Nathula Pass', 'Rumtek Monastery', 'Baba Harbhajan Singh Temple', 'Tsomgo Lake']
+var Pelling =  ['Pemayangtse Monastery']
+var Namchi =  ['Char Dham']
+var Ravangla =  ['Buddha Park']
+var Guwahati =  ['Kamakhya Temple', 'Umananda Island', 'Pobitora Wildlife Sanctuary']
+var Kaziranga =  ['Kaziranga National Park']
+var Sivasagar =  ['Sivasagar Sivadol']
+var Majuli =  ['Majuli Island']
+var Manas =  ['Manas National Park']
+var Hajo =  ['Hayagriva Madhava Temple']
+var Tawang =  ['Tawang Monastery']
+var Agartala =  ['Ujjayanta Palace']
+var Dumboor =  ['Dumboor Lake']
+var Unakoti =  ['Unakoti Rock Carvings']
+var Bastar =  ['Chitrakote Falls']
+var DzÃkouValley =  ['DzÃkou Valley']
+var Puducherry =  ['Promenade Beach', 'Paradise Beach']
+var Auroville =  ['Auroville']
+var PortBlair =  ['Cellular Jail']
+var HavelockIsland =  ['Radhanagar Beach']
+var NeilIsland =  ['Bharatpur Beach']
+var BaratangIsland =  ['Limestone Caves']
+var Diu =  ['Naida Caves', 'Diu Fort']
+var Deoghar =  ['Baba Baidyanath Temple']
+var Ranchi =  ['Pahari Mandir']
+var BodhGaya =  ['Mahabodhi Temple']
+var Patna =  ['Sanjay Gandhi Biological Park', 'Takhat Shri Harimandir Ji Patna Sahib', 'Budhha Smriti Park']
+var Gurugram =  ['Kingdom of Dreams', 'Ambience Mall', 'DLF CyberHub']
+var NewDelhi =  ['Gurudwara Bangla Sahib', 'Jama Masjid', 'Rail Museum']
+var Kedarnath =  ['Kedarnath']
+var GreaterNoida =  ['The Grand Venice Mall', 'Buddh International Circuit']
+var Bengaluru =  ['Wonderla Amusement Park', 'Orion Mall']
+var Cherrapunji =  ['Living Root Bridge']
+
     $("#inputDistrict").change(function(){
         var DistrictSelected = $(this).val();
         var optionsLists;
@@ -49,7 +256,53 @@ var SouthGoa=["Basilica of Bom Jesus","Dudhsagar Falls","Colva Beach","Dr. Salim
                 break;
           case "SouthGoa":
                 optionsLists = SouthGoa;
-                break;         
+                break;  
+          case "Ahmedabad":
+                optionsLists = Ahmedabad;
+                break;  
+          case "Junagadh":
+                optionsLists = Junagadh;
+                break;  
+          case "Bhuj":
+                optionsLists = Bhuj;
+                break;  
+          case "Dwarka":
+                optionsLists = Dwarka;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+          case "SouthGoa":
+                optionsLists = SouthGoa;
+                break;  
+
         }
 
         for(var i = 0; i < optionsLists.length; i++){
